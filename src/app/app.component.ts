878b1850dac5c6143105064a03b78249add88d6f
felixojiambo <felixojiamboe@gmail.com>
2024-11-24 19:18:04 +0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './shared/components/sidenav/sidenav.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, SidenavComponent, RouterOutlet, CommonModule],
+  imports: [HeaderComponent, SidenavComponent, RouterOutlet, CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
